44a8a09f912b990de6a2ff8f75e19ca624098a9d
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-09 17:58:28 -0300

    Add files via upload

---
+++ exercicio7/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] r = new int[5];
+int[] a = new int[10];
+int p = 0;
+int i;
+int j;
+
+Console.WriteLine("escreva os 5 numeros do gabarito: ");
+for (i = 0; i < 5; i++)
+{
+    Console.Write("escreva o numero:");
+    r[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("escreva os 10 numeros da aposta: ");
+for (i = 0; i < 10; i++)
+{
+    Console.Write("escreva o numero:");
+    a[i] = int.Parse(Console.ReadLine());
+}
+
+for (i = 0; i < 5; i++)
+{
+    for (j = 0; j < 10; j++)
+    {
+        if (r[i] == a[j])
+        {
+            p++;
+        }
+    }
+}
+
+Console.WriteLine("voce acertou " + p + " numeros");
---
+++ exercicio8/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] d = new int[20];
+int n;
+int c =0;
+
+Console.WriteLine("escreva 20 numeros:");
+for (int i = 0; i < 20; i++)
+{
+    Console.WriteLine("escreva os numeros: ");
+    d[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("escreva o valor n:");
+n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("vetor compactado em elementos maior que n:");
+for (int i = 0; i < 20; i++)
+{
+    if (d[i] > n)
+    {
+        Console.WriteLine(d[i]);
+        c++;
+    }
+}
+
+if (c == 0)
+{
+    Console.WriteLine("nenhum numero maior que n");
+}
